b5debef6319214b90e383573dfd5e39c69256e99
gregos3877 <parisien3877@hotmail.fr>
2018-11-16 16:59:44 +0100

    Ajout d'un nouveau post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent {
 
   posts = [new Post('Mon Premier Post', 'Lorem ipsum ...', 5 ),
            new Post('Mon Deuxième Post', 'Lorem ipsum ...', -5),
-           new Post('Un Autre Post', 'Lorem ipsum ...', 0)];
+           new Post('Un Autre Post', 'Lorem ipsum ...', 0),
+           new Post('Et Encore un petit', 'Il existe plusieurs post', 88)];
 
   constructor() {}
 }
